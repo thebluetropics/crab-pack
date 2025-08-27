5e87ef91b81a4173eecedfab0a771ad8dd3545b5
thebluetropics <thebluetropics@gmail.com>
2025-08-27 13:01:11 +0700

    fix fatal method lookup

--- mod/bytecode/method.py
+++ mod/bytecode/method.py
@@ -2,10 +2,7 @@ from .constant_pool import vcp_utf8_get
 
 def get_method(cf, cp, name, desc):
 	for m in cf[0x0d]:
-		name = vcp_utf8_get(cp, int.from_bytes(m[0x01]))
-		desc = vcp_utf8_get(cp, int.from_bytes(m[0x02]))
-
-		if not name.__eq__(name) or not desc.__eq__(desc):
+		if not vcp_utf8_get(cp, int.from_bytes(m[0x01])).__eq__(name) or not vcp_utf8_get(cp, int.from_bytes(m[0x02])).__eq__(desc):
 			continue
 
 		return m
