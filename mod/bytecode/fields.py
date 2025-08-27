a86bf0b110744fc386d1f3ecb5825d0acc2ade02
thebluetropics <thebluetropics@gmail.com>
2025-08-27 11:57:20 +0700

    add field creation function

--- mod/bytecode/fields.py
+++ mod/bytecode/fields.py
@@ -1,3 +1,15 @@
+field_access_flags = {
+	'public': 0x0001,
+	'private': 0x0002,
+	'protected': 0x0004,
+	'static': 0x0008,
+	'final': 0x0010,
+	'volatile': 0x0040,
+	'transient': 0x0080,
+	'synthetic': 0x1000,
+	'enum': 0x4000,
+}
+
 def push(cf, acc, name, desc, attrs):
 	f = [None] * 5
 
@@ -11,3 +23,20 @@ def push(cf, acc, name, desc, attrs):
 	f[0x04] = attrs
 
 	cf[0x0b].append(f)
+
+def make_field(acc_flags, name, desc):
+	acc = 0x0000
+
+	for flag in acc_flags:
+		acc = acc.__or__(field_access_flags[flag])
+
+	f = [None] * 5
+
+	f[0x00] = acc.to_bytes(2)
+	f[0x01] = name.to_bytes(2)
+	f[0x02] = desc.to_bytes(2)
+
+	f[0x03] = bytes(2)
+	f[0x04] = []
+
+	return f
