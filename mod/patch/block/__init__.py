d8427b062520f002fbcd545ec223ebaef6489838
thebluetropics <thebluetropics@gmail.com>
2025-08-30 05:46:16 +0700

    merge farmland block patch

--- mod/patch/block/__init__.py
+++ mod/patch/block/__init__.py
@@ -1,11 +1,11 @@
 from . import (farmland_block, block)
 
 def apply_client():
-	farmland_block.apply_client()
+	farmland_block.apply('client')
 	block.apply_client()
 
 def apply_server():
-	farmland_block.apply_server()
+	farmland_block.apply('server')
 	block.apply_server()
 
 __all__ = [apply_client.__name__, apply_server.__name__]
--- mod/patch/block/farmland_block.py
+++ mod/patch/block/farmland_block.py
@@ -1,43 +1,30 @@
 import mod
 
-from operator import eq
+from mod.bytecode.method import get_method
+from mod.bytecode.attribute import get_attribute
 from mod.bytecode import (
 	class_file,
-	constant_pool,
 	code_attribute,
-	instructions
+	instructions,
+	constant_pool
 )
 
-def apply_client():
-	if not mod.config.is_feature_enabled("no_crop_trampling"):
+def apply(side_name):
+	if not mod.config.is_feature_enabled('no_crop_trampling'):
 		return
 
-	cf = class_file.load(mod.config.path("stage/client/vl.class"))
-	cp = cf[0x04]
+	side = 0 if side_name.__eq__('client') else 1
 
-	# vl.b(Lfd;IIILsn;)V → onSteppedOn
-	m = None
-	for _m in cf[0x0d]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_m[0x01]))
-		desc = constant_pool.get_utf8(cp, int.from_bytes(_m[0x02]))
+	c_name = ['vl', 'no'][side]
+	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 
-		if eq(name, "b") and eq(desc, "(Lfd;IIILsn;)V"):
-			m = _m
-			break
-
-	a = None
-	for _a in m[0x04]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_a[0x00]))
-
-		if name.__eq__("Code"):
-			a = _a
-			break
+	m = get_method(cf, cf[0x04], 'b', ['(Lfd;IIILsn;)V', '(Ldj;IIILlq;)V'][side])
+	a = get_attribute(m[0x04], cf[0x04], 'Code')
 
 	a_code = code_attribute.load(a[0x02])
 
-	# empties the method body to effectively disable farmland trampling
 	a_code[0x03] = instructions.make(0, [
-		"return"
+		'return'
 	])
 
 	# update code length
@@ -46,10 +33,8 @@ def apply_client():
 	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
-	for i, _a in a_code[0x07]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_a[0x00]))
-
-		if eq(name, "LineNumberTable"):
+	for i, a in a_code[0x07]:
+		if constant_pool.get_utf8(cf[0x04], int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
 			del a_code[0x07][i]
 			break
 
@@ -59,59 +44,7 @@ def apply_client():
 	# update code attribute length
 	a[0x01] = len(a[0x02]).to_bytes(4)
 
-	with open(mod.config.path("stage/client/vl.class"), "wb") as f:
-		f.write(class_file.make(cf))
+	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
+		file.write(class_file.make(cf))
 
-	print("Patched client:vl.class → net.minecraft.block.FarmlandBlock")
-
-def apply_server():
-	if not mod.config.is_feature_enabled("no_crop_trampling"):
-		return
-
-	cf = class_file.load(mod.config.path("stage/server/no.class"))
-	cp = cf[0x04]
-
-	# na.b(Ldj;IIILlq;)V → onSteppedOn
-	m = None
-	for _m in cf[0x0d]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_m[0x01]))
-		desc = constant_pool.get_utf8(cp, int.from_bytes(_m[0x02]))
-
-		if eq(name, "b") and eq(desc, "(Ldj;IIILlq;)V"):
-			m = _m
-			break
-
-	a = None
-	for _a in m[0x04]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_a[0x00]))
-
-		if name.__eq__("Code"):
-			a = _a
-			break
-
-	a_code = code_attribute.load(a[0x02])
-
-	# empties the method body to effectively disable farmland trampling
-	a_code[0x02] = (1).to_bytes(4)
-	a_code[0x03] = b"\xb1"
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, _a in a_code[0x07]:
-		name = constant_pool.get_utf8(cp, int.from_bytes(_a[0x00]))
-
-		if eq(name, "LineNumberTable"):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = code_attribute.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path("stage/server/no.class"), "wb") as f:
-		f.write(class_file.make(cf))
-
-	print("Patched server:no.class → net.minecraft.block.FarmlandBlock")
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.block.FarmlandBlock')
