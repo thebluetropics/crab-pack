f08069376169211d1f7cd1626b13476b9ba3981e
thebluetropics <thebluetropics@gmail.com>
2025-09-01 21:12:03 +0700

    merge `Item` client and server patch

--- mod/patch/item/__init__.py
+++ mod/patch/item/__init__.py
@@ -2,10 +2,10 @@ from . import (food_item, item)
 
 def apply_client():
 	food_item.apply('client')
-	item.apply_client()
+	item.apply('client')
 
 def apply_server():
 	food_item.apply('server')
-	item.apply_server()
+	item.apply('server')
 
 __all__ = [apply_client.__name__]
--- mod/patch/item/item.py
+++ mod/patch/item/item.py
@@ -17,119 +17,92 @@ from mod.constant_pool import (
 	icpx_c
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('raw_squid_and_calamari'):
 		return
 
-	cf = class_file.load(mod.config.path('stage/client/gm.class'))
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['gm', 'ej'][side]
+
+	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
 
 	cf[0x0a] = (int.from_bytes(cf[0x0a]) + 2).to_bytes(2)
 	cf[0x0b].extend([
-		create_field(cf, cp_cache, ['public', 'static'], 'raw_squid', 'Lgm;'),
-		create_field(cf, cp_cache, ['public', 'static'], 'calamari', 'Lgm;')
+		create_field(cf, cp_cache, ['public', 'static'], 'raw_squid', f'L{c_name};'),
+		create_field(cf, cp_cache, ['public', 'static'], 'calamari', f'L{c_name};')
 	])
 
-	m = get_method(cf, cp_cache, '<clinit>', '()V')
-	a = get_attribute(m[0x04], cp_cache, 'Code')
+	_modify_static_initializer(cf, cp_cache, side_name)
 
-	a_code = attribute.code.load(a[0x02])
-
-	a_code[0x03] = a_code[0x03][0:2664] + instructions.assemble(2664, [
-		['new', icpx_c(cf, cp_cache, 'yw')],
-		'dup',
-		['sipush', 1024],
-		'iconst_1',
-		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'yw', '<init>', '(IIZ)V')],
-		'iconst_2',
-		['bipush', 15],
-		['invokevirtual', icpx_m(cf, cp_cache, 'yw', 'a', '(II)Lgm;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'raw_squid')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'gm', 'a', '(Ljava/lang/String;)Lgm;')],
-		['putstatic', icpx_f(cf, cp_cache, 'gm', 'raw_squid', 'Lgm;')],
-
-		['new', icpx_c(cf, cp_cache, 'yw')],
-		'dup',
-		['sipush', 1025],
-		'iconst_5',
-		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'yw', '<init>', '(IIZ)V')],
-		'iconst_3',
-		['bipush', 15],
-		['invokevirtual', icpx_m(cf, cp_cache, 'yw', 'a', '(II)Lgm;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'raw_squid')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'gm', 'a', '(Ljava/lang/String;)Lgm;')],
-		['putstatic', icpx_f(cf, cp_cache, 'gm', 'calamari', 'Lgm;')]
-	]) + a_code[0x03][2664:2668]
-
-	# update code length
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, a in a_code[0x07]:
-		if get_utf8_at(cp_cache, int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path('stage/client/gm.class'), 'wb') as f:
+	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as f:
 		f.write(class_file.assemble(cf))
 
-	print('Patched client:gm.class → net.minecraft.item.Item')
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.item.Item')
 
-def apply_server():
-	if not mod.config.is_feature_enabled('raw_squid_and_calamari'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/ej.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	cf[0x0a] = (int.from_bytes(cf[0x0a]) + 2).to_bytes(2)
-	cf[0x0b].extend([
-		create_field(cf, cp_cache, ['public', 'static'], 'raw_squid', 'Lej;'),
-		create_field(cf, cp_cache, ['public', 'static'], 'calamari', 'Lej;')
-	])
-
+def _modify_static_initializer(cf, cp_cache, side_name):
 	m = get_method(cf, cp_cache, '<clinit>', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
 	a_code = attribute.code.load(a[0x02])
 
-	a_code[0x03] = a_code[0x03][0:2664] + instructions.assemble(2664, [
-		['new', icpx_c(cf, cp_cache, 'px')],
-		'dup',
-		['sipush', 1024],
-		'iconst_1',
-		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'px', '<init>', '(IIZ)V')],
-		'iconst_2',
-		['bipush', 15],
-		['invokevirtual', icpx_m(cf, cp_cache, 'px', 'a', '(II)Lej;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'calamari')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'ej', 'a', '(Ljava/lang/String;)Lej;')],
-		['putstatic', icpx_f(cf, cp_cache, 'ej', 'raw_squid', 'Lej;')],
+	if side_name.__eq__('client'):
+		a_code[0x03] = a_code[0x03][0:2664] + instructions.assemble(2664, [
+			['new', icpx_c(cf, cp_cache, 'yw')],
+			'dup',
+			['sipush', 1024],
+			'iconst_1',
+			'iconst_0',
+			['invokespecial', icpx_m(cf, cp_cache, 'yw', '<init>', '(IIZ)V')],
+			'iconst_2',
+			['bipush', 15],
+			['invokevirtual', icpx_m(cf, cp_cache, 'yw', 'a', '(II)Lgm;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'raw_squid')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'gm', 'a', '(Ljava/lang/String;)Lgm;')],
+			['putstatic', icpx_f(cf, cp_cache, 'gm', 'raw_squid', 'Lgm;')],
 
-		['new', icpx_c(cf, cp_cache, 'px')],
-		'dup',
-		['sipush', 1025],
-		'iconst_5',
-		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'px', '<init>', '(IIZ)V')],
-		'iconst_3',
-		['bipush', 15],
-		['invokevirtual', icpx_m(cf, cp_cache, 'px', 'a', '(II)Lej;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'calamari')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'ej', 'a', '(Ljava/lang/String;)Lej;')],
-		['putstatic', icpx_f(cf, cp_cache, 'ej', 'calamari', 'Lej;')]
-	]) + a_code[0x03][2664:2668]
+			['new', icpx_c(cf, cp_cache, 'yw')],
+			'dup',
+			['sipush', 1025],
+			'iconst_5',
+			'iconst_0',
+			['invokespecial', icpx_m(cf, cp_cache, 'yw', '<init>', '(IIZ)V')],
+			'iconst_3',
+			['bipush', 15],
+			['invokevirtual', icpx_m(cf, cp_cache, 'yw', 'a', '(II)Lgm;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'raw_squid')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'gm', 'a', '(Ljava/lang/String;)Lgm;')],
+			['putstatic', icpx_f(cf, cp_cache, 'gm', 'calamari', 'Lgm;')]
+		]) + a_code[0x03][2664:2668]
+
+	if side_name.__eq__('server'):
+		a_code[0x03] = a_code[0x03][0:2664] + instructions.assemble(2664, [
+			['new', icpx_c(cf, cp_cache, 'px')],
+			'dup',
+			['sipush', 1024],
+			'iconst_1',
+			'iconst_0',
+			['invokespecial', icpx_m(cf, cp_cache, 'px', '<init>', '(IIZ)V')],
+			'iconst_2',
+			['bipush', 15],
+			['invokevirtual', icpx_m(cf, cp_cache, 'px', 'a', '(II)Lej;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'calamari')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'ej', 'a', '(Ljava/lang/String;)Lej;')],
+			['putstatic', icpx_f(cf, cp_cache, 'ej', 'raw_squid', 'Lej;')],
+
+			['new', icpx_c(cf, cp_cache, 'px')],
+			'dup',
+			['sipush', 1025],
+			'iconst_5',
+			'iconst_0',
+			['invokespecial', icpx_m(cf, cp_cache, 'px', '<init>', '(IIZ)V')],
+			'iconst_3',
+			['bipush', 15],
+			['invokevirtual', icpx_m(cf, cp_cache, 'px', 'a', '(II)Lej;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'calamari')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'ej', 'a', '(Ljava/lang/String;)Lej;')],
+			['putstatic', icpx_f(cf, cp_cache, 'ej', 'calamari', 'Lej;')]
+		]) + a_code[0x03][2664:2668]
 
 	# update code length
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
@@ -147,8 +120,3 @@ def apply_server():
 
 	# update code attribute length
 	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path('stage/server/ej.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
-
-	print('Patched server:ej.class → net.minecraft.item.Item')
