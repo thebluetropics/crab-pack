0e60b0eb04a7ea12968bcaea611b43c952265298
thebluetropics <thebluetropics@gmail.com>
2025-09-02 06:28:38 +0700

    merge `Packet` client and server patch

--- mod/patch/packet/__init__.py
+++ mod/patch/packet/__init__.py
@@ -5,10 +5,10 @@ from . import (
 
 def apply_client():
 	s2c_hunger_update.apply_client()
-	packet.apply_client()
+	packet.apply('client')
 
 def apply_server():
 	s2c_hunger_update.apply_server()
-	packet.apply_server()
+	packet.apply('server')
 
 __all__ = [apply_client.__name__, apply_server.__name__]
--- mod/patch/packet/packet.py
+++ mod/patch/packet/packet.py
@@ -14,67 +14,46 @@ from mod.constant_pool import (
 	icpx_c
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('hunger_and_thirst'):
 		return
 
-	cf = class_file.load(mod.config.path('stage/client/ki.class'))
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['ki', 'gt'][side]
+
+	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
 
+	_modify_static_initializer(cf, cp_cache, side_name)
+
+	with open(f'stage/{side_name}/{c_name}.class', 'wb') as file:
+		file.write(class_file.assemble(cf))
+
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.network.packet.Packet')
+
+def _modify_static_initializer(cf, cp_cache, side_name):
 	m = get_method(cf, cp_cache, '<clinit>', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
 	a_code = attribute.code.load(a[0x02])
 
-	a_code[0x03] = a_code[0x03][0:552] + instructions.assemble(0, [
-		['sipush', 201],
-		'iconst_1',
-		'iconst_1',
-		['ldc_w', icpx_c(cf, cp_cache, 'com/thebluetropics/crabpack/HungerUpdatePacket')],
-		['invokestatic', icpx_m(cf, cp_cache, 'ki', 'a', '(IZZLjava/lang/Class;)V')]
-	]) + a_code[0x03][552:567]
+	if side_name.__eq__('client'):
+		a_code[0x03] = a_code[0x03][0:552] + instructions.assemble(0, [
+			['sipush', 201],
+			'iconst_1',
+			'iconst_1',
+			['ldc_w', icpx_c(cf, cp_cache, 'com/thebluetropics/crabpack/HungerUpdatePacket')],
+			['invokestatic', icpx_m(cf, cp_cache, 'ki', 'a', '(IZZLjava/lang/Class;)V')]
+		]) + a_code[0x03][552:567]
 
-	# update code length
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, a in a_code[0x07]:
-		if get_utf8_at(cf, cp_cache, int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open('stage/client/ki.class', 'wb') as f:
-		f.write(class_file.assemble(cf))
-
-	print('Patched client:ki.class → net.minecraft.network.packet.Packet')
-
-def apply_server():
-	if not mod.config.is_feature_enabled('hunger_and_thirst'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/gt.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	m = get_method(cf, cp_cache, '<clinit>', '()V')
-	a = get_attribute(m[0x04], cp_cache, 'Code')
-
-	a_code = attribute.code.load(a[0x02])
-
-	a_code[0x03] = a_code[0x03][0:541] + instructions.assemble(0, [
-		['sipush', 201],
-		'iconst_1',
-		'iconst_1',
-		['ldc_w', icpx_c(cf, cp_cache, 'com/thebluetropics/crabpack/HungerUpdatePacket')],
-		['invokestatic', icpx_m(cf, cp_cache, 'gt', 'a', '(IZZLjava/lang/Class;)V')]
-	]) + a_code[0x03][541:566]
+	if side_name.__eq__('server'):
+		a_code[0x03] = a_code[0x03][0:541] + instructions.assemble(0, [
+			['sipush', 201],
+			'iconst_1',
+			'iconst_1',
+			['ldc_w', icpx_c(cf, cp_cache, 'com/thebluetropics/crabpack/HungerUpdatePacket')],
+			['invokestatic', icpx_m(cf, cp_cache, 'gt', 'a', '(IZZLjava/lang/Class;)V')]
+		]) + a_code[0x03][541:566]
 
 	# update code length
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
@@ -92,8 +71,3 @@ def apply_server():
 
 	# update code attribute length
 	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open('stage/server/gt.class', 'wb') as f:
-		f.write(class_file.assemble(cf))
-
-	print('Patched server:gt.class → net.minecraft.network.packet.Packet')
