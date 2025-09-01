ff909a8eb6027b8d073380adc654d3021cef326c
thebluetropics <thebluetropics@gmail.com>
2025-09-01 11:43:48 +0700

    merge Block client and server patch

--- mod/patch/block/__init__.py
+++ mod/patch/block/__init__.py
@@ -2,10 +2,10 @@ from . import (farmland_block, block)
 
 def apply_client():
 	farmland_block.apply('client')
-	block.apply_client()
+	block.apply('client')
 
 def apply_server():
 	farmland_block.apply('server')
-	block.apply_server()
+	block.apply('server')
 
 __all__ = [apply_client.__name__, apply_server.__name__]
--- mod/patch/block/block.py
+++ mod/patch/block/block.py
@@ -17,39 +17,63 @@ from mod.constant_pool import (
 	get_utf8_at
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('fortress_bricks'):
 		return
 
-	cf = class_file.load(mod.config.path('stage/client/uu.class'))
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['uu', 'na'][side]
+
+	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
 
 	cf[0x0a] = (int.from_bytes(cf[0x0a]) + 1).to_bytes(2)
-	cf[0x0b].append(create_field(cf, cp_cache, ['public', 'static'], 'fortress_bricks', 'Luu;'))
+	cf[0x0b].append(create_field(cf, cp_cache, ['public', 'static'], 'fortress_bricks', f'L{c_name};'))
 
 	m = get_method(cf, cp_cache, '<clinit>', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
 	a_code = attribute.code.load(a[0x02])
 
-	a_code[0x03] = a_code[0x03][0:3398] + instructions.assemble(3398, [
-		['new', icpx_c(cf, cp_cache, 'uu')],
-		'dup',
-		['bipush', 97],
-		['sipush', 166],
-		['getstatic', icpx_f(cf, cp_cache, 'ln', 'e', 'Lln;')],
-		['invokespecial', icpx_m(cf, cp_cache, 'uu', '<init>', '(IILln;)V')],
-		'fconst_2',
-		['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'c', '(F)Luu;')],
-		['ldc', 22],
-		['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'b', '(F)Luu;')],
-		['getstatic', icpx_f(cf, cp_cache, 'uu', 'h', 'Lct;')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'a', '(Lct;)Luu;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'fortress_bricks')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'a', '(Ljava/lang/String;)Luu;')],
-		['putstatic', icpx_f(cf, cp_cache, 'uu', 'fortress_bricks', 'Luu;')]
-	]) + a_code[0x03][3398:3678]
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
+	if side_name.__eq__('client'):
+		a_code[0x03] = a_code[0x03][0:3398] + instructions.assemble(3398, [
+			['new', icpx_c(cf, cp_cache, 'uu')],
+			'dup',
+			['bipush', 97],
+			['sipush', 166],
+			['getstatic', icpx_f(cf, cp_cache, 'ln', 'e', 'Lln;')],
+			['invokespecial', icpx_m(cf, cp_cache, 'uu', '<init>', '(IILln;)V')],
+			'fconst_2',
+			['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'c', '(F)Luu;')],
+			['ldc', 22],
+			['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'b', '(F)Luu;')],
+			['getstatic', icpx_f(cf, cp_cache, 'uu', 'h', 'Lct;')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'a', '(Lct;)Luu;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'fortress_bricks')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'uu', 'a', '(Ljava/lang/String;)Luu;')],
+			['putstatic', icpx_f(cf, cp_cache, 'uu', 'fortress_bricks', 'Luu;')]
+		]) + a_code[0x03][3398:3678]
+		a_code[0x02] = len(a_code[0x03]).to_bytes(4)
+
+	if side_name.__eq__('server'):
+		a_code[0x03] = a_code[0x03][0:3398] + instructions.assemble(3398, [
+			['new', icpx_c(cf, cp_cache, 'na')],
+			'dup',
+			['bipush', 97],
+			['sipush', 166],
+			['getstatic', icpx_f(cf, cp_cache, 'hj', 'e', 'Lhj;')],
+			['invokespecial', icpx_m(cf, cp_cache, 'na', '<init>', '(IILhj;)V')],
+			'fconst_2',
+			['invokevirtual', icpx_m(cf, cp_cache, 'na', 'c', '(F)Lna;')],
+			['ldc', 21],
+			['invokevirtual', icpx_m(cf, cp_cache, 'na', 'b', '(F)Lna;')],
+			['getstatic', icpx_f(cf, cp_cache, 'na', 'h', 'Lbu;')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'na', 'a', '(Lbu;)Lna;')],
+			['ldc_w', icpx_string(cf, cp_cache, 'fortress_bricks')],
+			['invokevirtual', icpx_m(cf, cp_cache, 'na', 'a', '(Ljava/lang/String;)Lna;')],
+			['putstatic', icpx_f(cf, cp_cache, 'na', 'fortress_bricks', 'Lna;')]
+		]) + a_code[0x03][3398:3678]
+		a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 
 	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
@@ -65,60 +89,7 @@ def apply_client():
 	# update code attribute length
 	a[0x01] = len(a[0x02]).to_bytes(4)
 
-	with open(mod.config.path('stage/client/uu.class'), 'wb') as file:
+	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
 		file.write(class_file.assemble(cf))
 
-	print('Patched client:uu.class → net.minecraft.block.Block')
-
-def apply_server():
-	if not mod.config.is_feature_enabled('fortress_bricks'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/na.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	cf[0x0a] = (int.from_bytes(cf[0x0a]) + 1).to_bytes(2)
-	cf[0x0b].append(create_field(cf, cp_cache, ['public', 'static'], 'fortress_bricks', 'Lna;'))
-
-	m = get_method(cf, cp_cache, '<clinit>', '()V')
-	a = get_attribute(m[0x04], cp_cache, 'Code')
-
-	a_code = attribute.code.load(a[0x02])
-
-	a_code[0x03] = a_code[0x03][0:3398] + instructions.assemble(3398, [
-		['new', icpx_c(cf, cp_cache, 'na')],
-		'dup',
-		['bipush', 97],
-		['sipush', 166],
-		['getstatic', icpx_f(cf, cp_cache, 'hj', 'e', 'Lhj;')],
-		['invokespecial', icpx_m(cf, cp_cache, 'na', '<init>', '(IILhj;)V')],
-		'fconst_2',
-		['invokevirtual', icpx_m(cf, cp_cache, 'na', 'c', '(F)Lna;')],
-		['ldc', 21],
-		['invokevirtual', icpx_m(cf, cp_cache, 'na', 'b', '(F)Lna;')],
-		['getstatic', icpx_f(cf, cp_cache, 'na', 'h', 'Lbu;')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'na', 'a', '(Lbu;)Lna;')],
-		['ldc_w', icpx_string(cf, cp_cache, 'fortress_bricks')],
-		['invokevirtual', icpx_m(cf, cp_cache, 'na', 'a', '(Ljava/lang/String;)Lna;')],
-		['putstatic', icpx_f(cf, cp_cache, 'na', 'fortress_bricks', 'Lna;')]
-	]) + a_code[0x03][3398:3678]
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, a in a_code[0x07]:
-		if get_utf8_at(cp_cache, int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path('stage/server/na.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
-
-	print('Patched server:na.class → net.minecraft.block.Block')
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.block.Block')
