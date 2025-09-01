c2ec25e34ca00ffaef5ca63e1406e530372076e6
thebluetropics <thebluetropics@gmail.com>
2025-09-02 06:33:43 +0700

    merge `NetworkHandler` client and server patch

--- mod/patch/network/__init__.py
+++ mod/patch/network/__init__.py
@@ -3,9 +3,9 @@ from . import (
 )
 
 def apply_client():
-	network_handler.apply_client()
+	network_handler.apply('client')
 
 def apply_server():
-	network_handler.apply_server()
+	network_handler.apply('server')
 
 __all__ = [apply_client.__name__, apply_server.__name__]
--- mod/patch/network/network_handler.py
+++ mod/patch/network/network_handler.py
@@ -9,59 +9,25 @@ from mod import (
 	constant_pool
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('hunger_and_thirst'):
 		return
 
-	cf = class_file.load(mod.config.path('stage/client/ti.class'))
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['ti', 'me'][side]
+
+	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
 
 	cf[0x0c] = (int.from_bytes(cf[0x0c]) + 1).to_bytes(2)
-	cf[0x0d].append(_on_hunger_update_client(cf, cp_cache))
+	cf[0x0d].append(_create_on_hunger_update_method(cf, cp_cache))
 
-	with open('stage/client/ti.class', 'wb') as file:
+	with open(f'stage/{side_name}/{c_name}.class', 'wb') as file:
 		file.write(class_file.assemble(cf))
 
-	print('Patched client:ti.class → net.minecraft.network.NetworkHandler')
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.network.NetworkHandler')
 
-def _on_hunger_update_client(cf, cp_cache):
-	m = create_method(cf, cp_cache, ['public'], 'onHungerUpdate', '(Lcom/thebluetropics/crabpack/HungerUpdatePacket;)V')
-
-	code = instructions.assemble(0, [
-		'return'
-	])
-	a_code = attribute.code.assemble([
-		(0).to_bytes(2),
-		(2).to_bytes(2),
-		len(code).to_bytes(4),
-		code,
-		(0).to_bytes(2),
-		[],
-		(0).to_bytes(2),
-		[]
-	])
-
-	m[0x03] = (1).to_bytes(2)
-	m[0x04] = [[i2cpx_utf8(cf, cp_cache, 'Code'), len(a_code).to_bytes(4), a_code]]
-
-	return m
-
-def apply_server():
-	if not mod.config.is_feature_enabled('hunger_and_thirst'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/me.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	cf[0x0c] = (int.from_bytes(cf[0x0c]) + 1).to_bytes(2)
-	cf[0x0d].append(_on_hunger_update_client(cf, cp_cache))
-
-	with open('stage/server/me.class', 'wb') as file:
-		file.write(class_file.assemble(cf))
-
-	print('Patched server:me.class → net.minecraft.network.NetworkHandler')
-
-def _on_hunger_update_client(cf, cp_cache):
+def _create_on_hunger_update_method(cf, cp_cache):
 	m = create_method(cf, cp_cache, ['public'], 'onHungerUpdate', '(Lcom/thebluetropics/crabpack/HungerUpdatePacket;)V')
 
 	code = instructions.assemble(0, [
