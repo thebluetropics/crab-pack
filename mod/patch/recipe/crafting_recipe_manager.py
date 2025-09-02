4adc9f705a783352c95d49725832546bd5f9f8b1
thebluetropics <thebluetropics@gmail.com>
2025-09-02 16:34:25 +0700

    add recipe for debugging

--- mod/patch/recipe/crafting_recipe_manager.py
+++ mod/patch/recipe/crafting_recipe_manager.py
@@ -18,9 +18,6 @@ from mod.constant_pool import (
 )
 
 def apply(side_name):
-	if not mod.config.is_feature_enabled('hunger_and_thirst'):
-		return
-
 	side = 0 if side_name.__eq__('client') else 1
 	c_name = ['hk', 'ey'][side]
 
@@ -39,63 +36,84 @@ def _modify_constructor(cf, cp_cache, side_name, side, c_name):
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
 	a_code = attribute.code.load(a[0x02])
-	ijct_code = [
-		'aload_0',
+	patch_code = []
 
-		['new', icpx_c(cf, cp_cache, ['iz', 'fy'][side])],
-		'dup',
-		['getstatic', icpx_f(cf, cp_cache, ['gm', 'ej'][side], 'BOTTLE', ['Lgm;', 'Lej;'][side])],
-		'iconst_1',
-		['invokespecial', icpx_m(cf, cp_cache, ['iz', 'fy'][side], '<init>', ['(Lgm;I)V', '(Lej;I)V'][side])],
+	if mod.config.is_feature_enabled('hunger_and_thirst'):
+		patch_code.extend([
+			'aload_0',
 
-		['bipush', 7],
-		['anewarray', icpx_c(cf, cp_cache, 'java/lang/Object')],
+			['new', icpx_c(cf, cp_cache, ['iz', 'fy'][side])],
+			'dup',
+			['getstatic', icpx_f(cf, cp_cache, ['gm', 'ej'][side], 'BOTTLE', ['Lgm;', 'Lej;'][side])],
+			'iconst_1',
+			['invokespecial', icpx_m(cf, cp_cache, ['iz', 'fy'][side], '<init>', ['(Lgm;I)V', '(Lej;I)V'][side])],
 
-		'dup',
-		'iconst_0',
-		['ldc_w', icpx_string(cf, cp_cache, ' a ')],
-		'aastore',
+			['bipush', 7],
+			['anewarray', icpx_c(cf, cp_cache, 'java/lang/Object')],
 
-		'dup',
-		'iconst_1',
-		['ldc_w', icpx_string(cf, cp_cache, 'b b')],
-		'aastore',
+			'dup',
+			'iconst_0',
+			['ldc_w', icpx_string(cf, cp_cache, ' a ')],
+			'aastore',
 
-		'dup',
-		'iconst_2',
-		['ldc_w', icpx_string(cf, cp_cache, ' b ')],
-		'aastore',
+			'dup',
+			'iconst_1',
+			['ldc_w', icpx_string(cf, cp_cache, 'b b')],
+			'aastore',
 
-		'dup',
-		'iconst_3',
-		['bipush', 97],
-		['invokestatic', icpx_m(cf, cp_cache, 'java/lang/Character', 'valueOf', '(C)Ljava/lang/Character;')],
-		'aastore',
+			'dup',
+			'iconst_2',
+			['ldc_w', icpx_string(cf, cp_cache, ' b ')],
+			'aastore',
 
-		'dup',
-		'iconst_4',
-		['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'y', ['Luu;', 'Lna;'][side])],
-		'aastore',
+			'dup',
+			'iconst_3',
+			['bipush', 97],
+			['invokestatic', icpx_m(cf, cp_cache, 'java/lang/Character', 'valueOf', '(C)Ljava/lang/Character;')],
+			'aastore',
 
-		'dup',
-		'iconst_5',
-		['bipush', 98],
-		['invokestatic', icpx_m(cf, cp_cache, 'java/lang/Character', 'valueOf', '(C)Ljava/lang/Character;')],
-		'aastore',
+			'dup',
+			'iconst_4',
+			['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'y', ['Luu;', 'Lna;'][side])],
+			'aastore',
 
-		'dup',
-		['bipush', 6],
-		['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'N', ['Luu;', 'Lna;'][side])],
-		'aastore',
+			'dup',
+			'iconst_5',
+			['bipush', 98],
+			['invokestatic', icpx_m(cf, cp_cache, 'java/lang/Character', 'valueOf', '(C)Ljava/lang/Character;')],
+			'aastore',
 
-		['invokevirtual', icpx_m(cf, cp_cache, c_name, 'a', ['(Liz;[Ljava/lang/Object;)V', '(Lfy;[Ljava/lang/Object;)V'][side])]
-	]
+			'dup',
+			['bipush', 6],
+			['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'N', ['Luu;', 'Lna;'][side])],
+			'aastore',
+
+			['invokevirtual', icpx_m(cf, cp_cache, c_name, 'a', ['(Liz;[Ljava/lang/Object;)V', '(Lfy;[Ljava/lang/Object;)V'][side])]
+		])
+
+	if mod.config.is_feature_enabled('debug_recipes'):
+		patch_code.extend([
+			'aload_0',
+			['new', icpx_c(cf, cp_cache, ['iz', 'fy'][side])],
+			'dup',
+			['getstatic', icpx_f(cf, cp_cache, ['gm', 'ej'][side], 'l', ['Lgm;', 'Lej;'][side])],
+			'iconst_1',
+			['bipush', 0],
+			['invokespecial', icpx_m(cf, cp_cache, ['iz', 'fy'][side], '<init>', ['(Lgm;II)V', '(Lej;II)V'][side])],
+			'iconst_1',
+			['anewarray', icpx_c(cf, cp_cache, 'java/lang/Object')],
+			'dup',
+			'iconst_0',
+			['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'w', ['Luu;', 'Lna;'][side])],
+			'aastore',
+			['invokevirtual', icpx_m(cf, cp_cache, c_name, 'b', ['(Liz;[Ljava/lang/Object;)V', '(Lfy;[Ljava/lang/Object;)V'][side])]
+		])
 
 	if side_name.__eq__('client'):
-		a_code[0x03] = a_code[0x03][0:3189] + instructions.assemble(0, ijct_code) + a_code[0x03][3189:3238]
+		a_code[0x03] = a_code[0x03][0:3189] + instructions.assemble(0, patch_code) + a_code[0x03][3189:3238]
 
 	if side_name.__eq__('server'):
-		a_code[0x03] = a_code[0x03][0:3189] + instructions.assemble(0, ijct_code) + a_code[0x03][3189:3238]
+		a_code[0x03] = a_code[0x03][0:3189] + instructions.assemble(0, patch_code) + a_code[0x03][3189:3238]
 
 	# update code length
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
