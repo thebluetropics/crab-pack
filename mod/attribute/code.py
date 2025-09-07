78a1059bf790e9e3ffe7a301b93ca70f4e6caaa7
thebluetropics <thebluetropics@gmail.com>
2025-09-07 11:12:59 +0700

    refactor `code.py`

--- mod/attribute/code.py
+++ mod/attribute/code.py
@@ -1,70 +1,57 @@
-# Load code attribute
-def load(seq):
+def load(a_code_bytes):
 	a_code = [None] * 8
 
-	a_code[0x00] = seq[0:2] # max stack
-	a_code[0x01] = seq[2:4] # max locals
+	a_code[0x00] = a_code_bytes[0:2]
+	a_code[0x01] = a_code_bytes[2:4]
 
-	# code length
-	a_code[0x02] = seq[4:8]
+	a_code[0x02] = a_code_bytes[4:8]
 	code_length = int.from_bytes(a_code[0x02])
 
-	# code
-	a_code[0x03] = seq[8:8 + code_length]
+	a_code[0x03] = a_code_bytes[8:8 + code_length]
 	i = 8 + code_length
 
-	# exception table length
-	a_code[0x04] = seq[i:i + 2]
+	a_code[0x04] = a_code_bytes[i:i + 2]
 	excp_table_len = int.from_bytes(a_code[0x04])
 	i += 2
 
-	# exception table
 	excp_table = []
+	a_code[0x05] = excp_table
 
 	for _ in range(excp_table_len):
 		excp_table.append([
-			seq[i:i + 2],
-			seq[i + 2:i + 4],
-			seq[i + 4:i + 6],
-			seq[i + 6:i + 8]
+			a_code_bytes[i:i + 2],
+			a_code_bytes[i + 2:i + 4],
+			a_code_bytes[i + 4:i + 6],
+			a_code_bytes[i + 6:i + 8]
 		])
 		i += 8
 
-	a_code[0x05] = excp_table
-
-	# attribute count
-	a_code[0x06] = seq[i:i + 2]
+	a_code[0x06] = a_code_bytes[i:i + 2]
 	attr_count = int.from_bytes(a_code[0x06])
 	i += 2
 
-	# attributes
 	attr_list = []
+	a_code[0x07] = attr_list
 
 	for _ in range(attr_count):
 		attr = [
-			seq[i:i + 2], # → name index
-			seq[i + 2:i + 6], # → length
+			a_code_bytes[i:i + 2],
+			a_code_bytes[i + 2:i + 6],
 			None
 		]
 		length = int.from_bytes(attr[0x01])
 
-		attr[0x02] = seq[i + 6:i + 6 + length]
+		attr[0x02] = a_code_bytes[i + 6:i + 6 + length]
 		i += 6 + length
-
-	a_code[0x07] = attr_list
 
 	return a_code
 
-# Assemble code attribute
 def assemble(a_code):
-	seq = bytearray()
-
-	for x in [
-		*[a_code[i] for i in range(0x00, 0x05)],
-		*[bytes().join(entry) for entry in a_code[0x05]],
+	return b''.join([
+		a_code[0x00], a_code[0x01],
+		a_code[0x02], a_code[0x03],
+		a_code[0x04],
+		*[b''.join(entry) for entry in a_code[0x05]],
 		a_code[0x06],
-		*[bytes().join(a) for a in a_code[0x07]]
-	]:
-		seq.extend(x)
-
-	return memoryview(seq).tobytes()
+		*[b''.join(a) for a in a_code[0x07]]
+	])
