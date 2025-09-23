ee306a3822c35966158b2c50bbf963d80a379913
thebluetropics <thebluetropics@gmail.com>
2025-09-23 22:50:12 +0700

    refactor `world.py`

--- mod/patch/world/__init__.py
+++ mod/patch/world/__init__.py
@@ -1,9 +1,9 @@
 from . import world
 
 def apply_client():
-	world.apply_client()
+	world.apply('client')
 
 def apply_server():
-	world.apply_server()
+	world.apply('server')
 
 __all__ = [apply_client.__name__, apply_server.__name__]
--- mod/patch/world/world.py
+++ mod/patch/world/world.py
@@ -1,38 +1,44 @@
 import mod
 
-from mod.attribute import get_attribute
-from mod.method import get_method
-from mod import (
-	class_file,
-	attribute,
-	constant_pool
+from modmaker.a import (
+	get_attribute
 )
-from mod.constant_pool import (
-	i2cpx_long,
+from modmaker.a_code import (
+	a_code_load,
+	a_code_assemble,
+	assemble_code
+)
+from modmaker.m import(
+	get_method
+)
+from modmaker.cf import (
+	load_class_file,
+	cf_assemble
+)
+from modmaker.cp import (
+	cp_init_cache,
 	get_utf8_at
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('debug.fixed_time'):
 		return
 
-	cf = class_file.load(mod.config.path('stage/client/fd.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['fd', 'dj'][side]
 
-	m = get_method(cf, cp_cache, 'l', '()V') # → tick
+	cf = load_class_file(mod.config.path(f'stage/{side_name}/{c_name}.class'))
+	cp_cache = cp_init_cache(cf[0x04])
+
+	m = get_method(cf, cp_cache, ['l', 'h'][side], '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
-	a_code = attribute.code.load(a[0x02])
+	a_code = a_code_load(a[0x02])
+	code_length = len(a_code[0x03])
 
-	code = bytearray(a_code[0x03])
-	del code[184]
-	code[184:184] = bytes().join([
-		b'\x14' + i2cpx_long(cf, cp_cache, 6000)
-	])
-	a_code[0x03] = bytes(code)
+	a_code[0x03] = a_code[0x03][0:184] + assemble_code(cf, cp_cache, side, 184, [['ldc2_w.i64', 6000]]) + a_code[0x03][185:code_length]
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 
-	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -40,52 +46,10 @@ def apply_client():
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
 
-	with open(mod.config.path('stage/client/fd.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
+	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
+		file.write(cf_assemble(cf))
 
-	print('Patched client:fd.class → net.minecraft.world.World')
-
-def apply_server():
-	if not mod.config.is_feature_enabled('debug.fixed_time'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/dj.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	m = get_method(cf, cp_cache, 'h', '()V') # → tick
-	a = get_attribute(m[0x04], cp_cache, 'Code')
-
-	a_code = attribute.code.load(a[0x02])
-
-	code = bytearray(a_code[0x03])
-	del code[184]
-	code[184:184] = bytes().join([
-		b'\x14' + i2cpx_long(cf, cp_cache, 6000)
-	])
-	a_code[0x03] = bytes(code)
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, a in a_code[0x07]:
-		if get_utf8_at(cp_cache, int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path('stage/server/dj.class'), 'wb') as f:
-		f.write(class_file.assemble(cf))
-
-	print('Patched server:dj.class → net.minecraft.world.World')
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.world.World')
