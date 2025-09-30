496871821676b9ba1368d518ef668d32794f27c0
thebluetropics <thebluetropics@gmail.com>
2025-09-30 23:41:10 +0700

    refactor `solid_grass_block.py`

--- mod/patch/block/solid_grass_block.py
+++ mod/patch/block/solid_grass_block.py
@@ -1,21 +1,20 @@
 import os, mod
 
-from mod.method import create_method
-from mod import (
-	attribute,
-	instructions,
-	constant_pool
+from modmaker.a_code import (
+	a_code_assemble,
+	assemble_code
 )
-from mod.class_file import (
-	create_class_file,
-	assemble_class_file
+from modmaker.cf import (
+	cf_assemble,
+	cf_create
 )
-from mod.constant_pool import (
-	icpx_f,
-	icpx_m,
+from modmaker.cp import (
+	cp_init_cache,
 	i2cpx_utf8,
-	i2cpx_c,
-	icpx_i
+	i2cpx_c
+)
+from modmaker.m import (
+	create_method
 )
 
 def apply(side_name):
@@ -25,8 +24,8 @@ def apply(side_name):
 	side = 0 if side_name.__eq__('client') else 1
 	c_name = 'com/thebluetropics/crabpack/SolidGrassBlock'
 
-	cf = create_class_file()
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
+	cf = cf_create()
+	cp_cache = cp_init_cache(cf[0x04])
 
 	cf[0x05] = (0x0001.__or__(0x0020)).to_bytes(2)
 	cf[0x06] = i2cpx_c(cf, cp_cache, c_name)
@@ -46,24 +45,24 @@ def apply(side_name):
 		os.makedirs(f'stage/{side_name}/com/thebluetropics/crabpack', exist_ok=True)
 
 	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
-		file.write(assemble_class_file(cf))
+		file.write(cf_assemble(cf))
 
 	print(f'Created {side_name}:SolidGrassBlock.class')
 
 def _create_constructor(cf, cp_cache, side, c_name):
 	m = create_method(cf, cp_cache, ['public'], '<init>', '(I)V')
 
-	code = instructions.assemble(0, [
+	code = assemble_code(cf, cp_cache, side, 0, [
 		'aload_0',
 		'iload_1',
-		['getstatic', icpx_f(cf, cp_cache, ['ln', 'hj'][side], 'b', ['Lln;', 'Lhj;'][side])],
-		['invokespecial', icpx_m(cf, cp_cache, ['uu', 'na'][side], '<init>', ['(ILln;)V', '(ILhj;)V'][side])],
+		['getstatic', ('ln', 'hj'), 'b', ('Lln;', 'Lhj;')],
+		['invokespecial', ('uu', 'na'), '<init>', ('(ILln;)V', '(ILhj;)V')],
 		'aload_0',
 		'iconst_0',
-		['putfield', icpx_f(cf, cp_cache, c_name, 'bm', 'I')],
+		['putfield', c_name, 'bm', 'I'],
 		'return'
 	])
-	a_code = attribute.code.assemble([
+	a_code = a_code_assemble([
 		(3).to_bytes(2),
 		(2).to_bytes(2),
 		len(code).to_bytes(4),
@@ -82,37 +81,37 @@ def _create_constructor(cf, cp_cache, side, c_name):
 def _create_get_color_multiplier_method(cf, cp_cache):
 	m = create_method(cf, cp_cache, ['public'], 'b', '(Lxp;III)I')
 
-	code = instructions.assemble(0, [
+	code = assemble_code(cf, cp_cache, 0, 0, [
 		'aload_1',
-		['invokeinterface', icpx_i(cf, cp_cache, 'xp', 'a', '()Lxv;'), 1],
+		['invokeinterface', 'xp', 'a', '()Lxv;', 1],
 		['iload', 2],
 		['iload', 4],
 		'iconst_1',
 		'iconst_1',
-		['invokevirtual', icpx_m(cf, cp_cache, 'xv', 'a', '(IIII)[Lkd;')],
+		['invokevirtual', 'xv', 'a', '(IIII)[Lkd;'],
 		'pop',
 
 		'aload_1',
-		['invokeinterface', icpx_i(cf, cp_cache, 'xp', 'a', '()Lxv;'), 1],
-		['getfield', icpx_f(cf, cp_cache, 'xv', 'a', '[D')],
+		['invokeinterface', 'xp', 'a', '()Lxv;', 1],
+		['getfield', 'xv', 'a', '[D'],
 		'iconst_0',
 		'daload',
 		['dstore', 5],
 
 		'aload_1',
-		['invokeinterface', icpx_i(cf, cp_cache, 'xp', 'a', '()Lxv;'), 1],
-		['getfield', icpx_f(cf, cp_cache, 'xv', 'b', '[D')],
+		['invokeinterface', 'xp', 'a', '()Lxv;', 1],
+		['getfield', 'xv', 'b', '[D'],
 		'iconst_0',
 		'daload',
 		['dstore', 7],
 
 		['dload', 5],
 		['dload', 7],
-		['invokestatic', icpx_m(cf, cp_cache, 'ia', 'a', '(DD)I')],
+		['invokestatic', 'ia', 'a', '(DD)I'],
 
 		'ireturn'
 	])
-	a_code = attribute.code.assemble([
+	a_code = a_code_assemble([
 		(5).to_bytes(2),
 		(9).to_bytes(2),
 		len(code).to_bytes(4),
@@ -131,14 +130,14 @@ def _create_get_color_multiplier_method(cf, cp_cache):
 def _create_get_dropped_item_id_method(cf, cp_cache, side):
 	m = create_method(cf, cp_cache, ['public'], 'a', '(ILjava/util/Random;)I')
 
-	code = instructions.assemble(0, [
-    ['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'w', ['Luu;', 'Lna;'][side])],
+	code = assemble_code(cf, cp_cache, side, 0, [
+    ['getstatic', ('uu', 'na'), 'w', ('Luu;', 'Lna;')],
     'iconst_0',
     ['aload', 2],
-    ['invokevirtual', icpx_m(cf, cp_cache, ['uu', 'na'][side], 'a', '(ILjava/util/Random;)I')],
+    ['invokevirtual', ('uu', 'na'), 'a', '(ILjava/util/Random;)I'],
 		'ireturn'
 	])
-	a_code = attribute.code.assemble([
+	a_code = a_code_assemble([
 		(3).to_bytes(2),
 		(3).to_bytes(2),
 		len(code).to_bytes(4),
