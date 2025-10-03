35815434f85d3de068301321a28662419492afcb
thebluetropics <thebluetropics@gmail.com>
2025-10-04 02:46:23 +0700

    refactor `pickaxe_item.py`

--- mod/patch/item/pickaxe_item.py
+++ mod/patch/item/pickaxe_item.py
@@ -1,16 +1,23 @@
 import mod
 
-from mod.attribute import get_attribute
-from mod.method import get_method
-from mod.constant_pool import get_utf8_at
-from mod import (
-	class_file,
-	attribute,
-	constant_pool,
-	instructions
+from modmaker.a import (
+	get_attribute
 )
-from mod.constant_pool import (
-	icpx_f
+from modmaker.a_code import (
+	a_code_load,
+	a_code_assemble,
+	assemble_code
+)
+from modmaker.m import(
+	get_method
+)
+from modmaker.cf import (
+	load_class_file,
+	cf_assemble
+)
+from modmaker.cp import (
+	cp_init_cache,
+	get_utf8_at
 )
 
 def apply(side_name):
@@ -20,13 +27,13 @@ def apply(side_name):
 	side = 0 if side_name.__eq__('client') else 1
 	c_name = ['au', 'ah'][side]
 
-	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
+	cf = load_class_file(mod.config.path(f'stage/{side_name}/{c_name}.class'))
+	cp_cache = cp_init_cache(cf[0x04])
 
 	_modify_static_initializer(cf, cp_cache, side)
 
 	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
+		file.write(cf_assemble(cf))
 
 	print(f'Patched {side_name}:{c_name}.class → net.minecraft.item.PickaxeItem')
 
@@ -34,13 +41,13 @@ def _modify_static_initializer(cf, cp_cache, side):
 	m = get_method(cf, cp_cache, '<clinit>', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
-	a_code = attribute.code.load(a[0x02])
+	a_code = a_code_load(a[0x02])
 
 	count = 18
 	ext_code = [
 		'dup',
 		['bipush', 17],
-		['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'aC', ['Luu;', 'Lna;'][side])],
+		['getstatic', ('uu', 'na'), 'aC', ('Luu;', 'Lna;')],
 		'aastore'
 	]
 
@@ -48,23 +55,22 @@ def _modify_static_initializer(cf, cp_cache, side):
 		ext_code.extend([
 			'dup',
 			['bipush', count],
-			['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'FORTRESS_BRICKS', ['Luu;', 'Lna;'][side])],
+			['getstatic', ('uu', 'na'), 'FORTRESS_BRICKS', ('Luu;', 'Lna;')],
 			'aastore'
 		])
 		ext_code.extend([
 			'dup',
 			['bipush', count + 1],
-			['getstatic', icpx_f(cf, cp_cache, ['uu', 'na'][side], 'LIGHT_FORTRESS_BRICKS', ['Luu;', 'Lna;'][side])],
+			['getstatic', ('uu', 'na'), 'LIGHT_FORTRESS_BRICKS', ('Luu;', 'Lna;')],
 			'aastore'
 		])
 		count += 2
 
-	a_code[0x03] = instructions.assemble(0, [
+	a_code[0x03] = assemble_code(cf, cp_cache, side, 0, [
 		['sipush', count]
-	]) + a_code[0x03][2:118] + instructions.assemble(121, ext_code) + a_code[0x03][118:122]
+	]) + a_code[0x03][2:118] + assemble_code(cf, cp_cache, side, 121, ext_code) + a_code[0x03][118:122]
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 
-	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -72,8 +78,5 @@ def _modify_static_initializer(cf, cp_cache, side):
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
