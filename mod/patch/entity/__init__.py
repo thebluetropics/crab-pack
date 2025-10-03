5b38c6b17e68c6b5ab9756ff1bad057db2200f8b
thebluetropics <thebluetropics@gmail.com>
2025-10-03 15:19:05 +0700

    refactor `squid_entity.py`

--- mod/patch/entity/__init__.py
+++ mod/patch/entity/__init__.py
@@ -2,11 +2,11 @@ from . import (player_entity, squid_entity, server_player_entity)
 
 def apply_client():
 	player_entity.apply('client')
-	squid_entity.apply_client()
+	squid_entity.apply('client')
 
 def apply_server():
 	player_entity.apply('server')
-	squid_entity.apply_server()
+	squid_entity.apply('server')
 	server_player_entity.apply()
 
 __all__ = [apply_client.__name__]
--- mod/patch/entity/squid_entity.py
+++ mod/patch/entity/squid_entity.py
@@ -1,49 +1,54 @@
 import mod
 
-from mod.attribute import get_attribute
-from mod.method import get_method
-from mod import (
-	class_file,
-	attribute,
-	instructions,
-	constant_pool
+from modmaker.a import (
+	get_attribute
 )
-from mod.constant_pool import (
-	icpx_f,
-	icpx_m,
-	get_utf8_at,
-	icpx_c
+from modmaker.a_code import (
+	a_code_load,
+	a_code_assemble,
+	assemble_code
+)
+from modmaker.m import(
+	get_method
+)
+from modmaker.cf import (
+	load_class_file,
+	cf_assemble
+)
+from modmaker.cp import (
+	cp_init_cache,
+	get_utf8_at
 )
 
-def apply_client():
+def apply(side_name):
 	if not mod.config.is_feature_enabled('food.raw_squid_and_calamari'):
-		return
+		pass
 
-	cf = class_file.load(mod.config.path('stage/client/xt.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
+	side = 0 if side_name.__eq__('client') else 1
+	c_name = ['xt', 'pf'][side]
+
+	cf = load_class_file(mod.config.path(f'stage/{side_name}/{c_name}.class'))
+	cp_cache = cp_init_cache(cf[0x04])
 
 	m = get_method(cf, cp_cache, 'q', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
-	a_code = attribute.code.load(a[0x02])
+	a_code = a_code_load(a[0x02])
 
-	a_code[0x03] = a_code[0x03][0:42] + instructions.assemble(42, [
+	a_code[0x03] = a_code[0x03][0:42] + assemble_code(cf, cp_cache, side, 42, [
 		'aload_0',
-		['new', icpx_c(cf, cp_cache, 'iz')],
+		['new', ('iz', 'fy')],
 		'dup',
-		['getstatic', icpx_f(cf, cp_cache, 'gm', 'raw_squid', 'Lgm;')],
+		['getstatic', ('gm', 'ej'), 'raw_squid', ('Lgm;', 'Lej;')],
 		'iconst_1',
 		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'iz', '<init>', '(Lgm;II)V')],
+		['invokespecial', ('iz', 'fy'), '<init>', ('(Lgm;II)V', '(Lej;II)V')],
 		'fconst_0',
-		['invokevirtual', icpx_m(cf, cp_cache, 'xt', 'a', '(Liz;F)Lhl;')],
+		['invokevirtual', c_name, 'a', ('(Liz;F)Lhl;', '(Lfy;F)Lez;')],
 		'return'
 	])
 
-	# update code length
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -51,60 +56,10 @@ def apply_client():
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
 
-	with open(mod.config.path('stage/client/xt.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
+	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
+		file.write(cf_assemble(cf))
 
-	print('Patched client:xt.class → net.minecraft.entity.SquidEntity')
-
-def apply_server():
-	if not mod.config.is_feature_enabled('raw_squid_and_calamari'):
-		return
-
-	cf = class_file.load(mod.config.path('stage/server/pf.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
-
-	m = get_method(cf, cp_cache, 'q', '()V')
-	a = get_attribute(m[0x04], cp_cache, 'Code')
-
-	a_code = attribute.code.load(a[0x02])
-
-	a_code[0x03] = a_code[0x03][0:42] + instructions.assemble(42, [
-		'aload_0',
-		['new', icpx_c(cf, cp_cache, 'fy')],
-		'dup',
-		['getstatic', icpx_f(cf, cp_cache, 'ej', 'raw_squid', 'Lej;')],
-		'iconst_1',
-		'iconst_0',
-		['invokespecial', icpx_m(cf, cp_cache, 'fy', '<init>', '(Lej;II)V')],
-		'fconst_0',
-		['invokevirtual', icpx_m(cf, cp_cache, 'pf', 'a', '(Lfy;F)Lez;')],
-		'return'
-	])
-
-	# update code length
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
-
-	# remove line number table
-	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
-
-	for i, a in a_code[0x07]:
-		if get_utf8_at(cp_cache, int.from_bytes(a[0x00])).__eq__('LineNumberTable'):
-			del a_code[0x07][i]
-			break
-
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
-	a[0x01] = len(a[0x02]).to_bytes(4)
-
-	with open(mod.config.path('stage/server/pf.class'), 'wb') as f:
-		f.write(class_file.assemble(cf))
-
-	print('Patched server:pf.class → net.minecraft.entity.SquidEntity')
+	print(f'Patched {side_name}:{c_name}.class → net.minecraft.entity.SquidEntity')
