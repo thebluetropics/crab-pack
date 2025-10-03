8df8736d6e150c592790ab86e932b32e7774cd08
thebluetropics <thebluetropics@gmail.com>
2025-10-04 02:52:47 +0700

    refactor `food_item.py`

--- mod/patch/item/food_item.py
+++ mod/patch/item/food_item.py
@@ -1,45 +1,56 @@
 import mod
 
-from mod.attribute import get_attribute
-from mod.method import get_method
-from mod.field import create_field
-from mod.constant_pool import get_utf8_at
-from mod import (
-	class_file,
-	attribute,
-	constant_pool,
-	instructions
+from modmaker.a import (
+	get_attribute
 )
-from mod.constant_pool import (
-	icpx_f,
-	icpx_m
+from modmaker.a_code import (
+	a_code_load,
+	a_code_assemble,
+	assemble_code
+)
+from modmaker.m import(
+	get_method
+)
+from modmaker.f import(
+	create_field
+)
+from modmaker.cf import (
+	load_class_file,
+	cf_assemble
+)
+from modmaker.cp import (
+	cp_init_cache,
+	get_utf8_at
 )
 
 def apply(side_name):
+	if not mod.config.is_one_of_features_enabled(['etc.hunger_and_thirst', 'food.stackable_food']):
+		return
+
 	side = 0 if side_name.__eq__('client') else 1
 	c_name = ['yw', 'px'][side]
 
-	cf = class_file.load(mod.config.path(f'stage/{side_name}/{c_name}.class'))
-	cp_cache = constant_pool.init_constant_pool_cache(cf[0x04])
+	cf = load_class_file(mod.config.path(f'stage/{side_name}/{c_name}.class'))
+	cp_cache = cp_init_cache(cf[0x04])
 
-	cf[0x0a] = (int.from_bytes(cf[0x0a]) + 1).to_bytes(2)
 	cf[0x0b].append(create_field(cf, cp_cache, ['public', 'final'], 'hungerRestored', 'I'))
+	cf[0x0a] = len(cf[0x0b]).to_bytes(2)
 
-	_modify_constructor(cf, cp_cache, c_name)
+	_modify_constructor(cf, cp_cache, side, c_name)
 
 	if mod.config.is_feature_enabled('etc.hunger_and_thirst'):
 		_modify_use_method(cf, cp_cache, side, c_name)
 
 	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
-		file.write(class_file.assemble(cf))
+		file.write(cf_assemble(cf))
 
 	print(f'Patched {side_name}:{c_name}.class → net.minecraft.item.FoodItem')
 
-def _modify_constructor(cf, cp_cache, c_name):
+def _modify_constructor(cf, cp_cache, side, c_name):
 	m = get_method(cf, cp_cache, '<init>', '(IIZ)V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
-	a_code = attribute.code.load(a[0x02])
+	a_code = a_code_load(a[0x02])
 
 	patch_code_0 = ['iconst_1'] if not mod.config.is_feature_enabled('food.stackable_food') else [['bipush', 16]]
 	patch_code_1 = None
@@ -48,16 +59,15 @@ def _modify_constructor(cf, cp_cache, c_name):
 		patch_code_1 = [
 			'aload_0',
 			['bipush', 50],
-			['putfield', icpx_f(cf, cp_cache, c_name, 'hungerRestored', 'I')],
+			['putfield', c_name, 'hungerRestored', 'I'],
 			'return'
 		]
 	else:
 		patch_code_1 = ['return']
 
-	a_code[0x03] = a_code[0x03][0:16] + instructions.assemble(16, patch_code_0) + a_code[0x03][17:20] + instructions.assemble(20, patch_code_1)
+	a_code[0x03] = a_code[0x03][0:16] + assemble_code(cf, cp_cache, side, 16, patch_code_0) + a_code[0x03][17:20] + assemble_code(cf, cp_cache, side, 20, patch_code_1)
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 
-	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -65,27 +75,23 @@ def _modify_constructor(cf, cp_cache, c_name):
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
 
 def _modify_use_method(cf, cp_cache, side, c_name):
 	m = get_method(cf, cp_cache, 'a', ['(Liz;Lfd;Lgs;)Liz;', '(Lfy;Ldj;Lem;)Lfy;'][side])
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
-	a_code = attribute.code.load(a[0x02])
+	a_code = a_code_load(a[0x02])
 
-	a_code[0x03] = a_code[0x03][0:18] + instructions.assemble(0, [
+	a_code[0x03] = a_code[0x03][0:18] + assemble_code(cf, cp_cache, side, 0, [
 		'aload_3',
 		'aload_0',
-		['getfield', icpx_f(cf, cp_cache, c_name, 'hungerRestored', 'I')],
-		['invokevirtual', icpx_m(cf, cp_cache, ['gs', 'em'][side], 'restoreHunger', '(I)V')],
+		['getfield', c_name, 'hungerRestored', 'I'],
+		['invokevirtual', ('gs', 'em'), 'restoreHunger', '(I)V'],
 	]) + a_code[0x03][18:20]
 	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 
-	# remove line number table
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -93,8 +99,5 @@ def _modify_use_method(cf, cp_cache, side, c_name):
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = attribute.code.assemble(a_code)
-
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
