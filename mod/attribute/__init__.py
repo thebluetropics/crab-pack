005df06bc3bb88de9b1c790c3de6d5ca5cb8d404
thebluetropics <thebluetropics@gmail.com>
2025-10-04 08:37:56 +0700

    remove obsolete code

--- mod/attribute/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import code
-from ..constant_pool import get_utf8_at
-
-def get_attribute(attributes, xcp, name):
-	for a in attributes:
-		if get_utf8_at(xcp, int.from_bytes(a[0x00])).__eq__(name):
-			return a
-
-	return None
