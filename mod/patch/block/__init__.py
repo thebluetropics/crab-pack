d9569a178e3db6ae92e890e11ada096506fc5fca
thebluetropics <thebluetropics@gmail.com>
2025-10-16 17:32:28 +0700

    refactor `block_entity.py`
    Refactor `block_entity.py` to make it more organized and better compatibility.

--- mod/patch/block/__init__.py
+++ mod/patch/block/__init__.py
@@ -7,11 +7,11 @@ def apply_client():
 	solid_grass_block.apply('client')
 	plant_block.apply('client')
 	mortar.apply('client')
+	block_entity.apply('client', 0)
 
 	if mod.config.is_feature_enabled('block.smelter'):
 		smelter_block.apply('client')
 		smelter_block_entity.apply('client')
-		block_entity.apply('client')
 
 	if mod.config.is_feature_enabled('block.persistent_leaves'):
 		persistent_leaves_block.apply('client')
@@ -23,11 +23,11 @@ def apply_server():
 	solid_grass_block.apply('server')
 	plant_block.apply('server')
 	mortar.apply('server')
+	block_entity.apply('server', 1)
 
 	if mod.config.is_feature_enabled('block.smelter'):
 		smelter_block.apply('server')
 		smelter_block_entity.apply('server')
-		block_entity.apply('server')
 
 	if mod.config.is_feature_enabled('block.persistent_leaves'):
 		persistent_leaves_block.apply('server')
--- mod/patch/block/block_entity.py
+++ mod/patch/block/block_entity.py
@@ -1,37 +1,38 @@
 from modmaker import *
 import mod
 
-def apply(side_name):
-	side = 0 if side_name.__eq__('client') else 1
-	c_name = ['ow', 'jh'][side]
+def apply(side_name, side):
+	if mod.config.is_one_of_features_enabled(['block.smelter']):
+		return
 
+	c_name = ('ow', 'jh')[side]
 	cf = load_class_file(mod.config.path(f'stage/{side_name}/{c_name}.class'))
 	cp_cache = cp_init_cache(cf[0x04])
 
-	_modify_static_initializer(cf, cp_cache, side, c_name)
+	modify_static_initializer(cf, cp_cache, side, c_name)
 
 	with open(mod.config.path(f'stage/{side_name}/{c_name}.class'), 'wb') as file:
 		file.write(cf_assemble(cf))
 
 	print(f'Patched {side_name}:{c_name}.class → net.minecraft.block.entity.BlockEntity')
 
-def _modify_static_initializer(cf, cp_cache, side, c_name):
+def modify_static_initializer(cf, cp_cache, side, c_name):
 	m = get_method(cf, cp_cache, '<clinit>', '()V')
 	a = get_attribute(m[0x04], cp_cache, 'Code')
 
 	a_code = a_code_load(a[0x02])
+	code = []
 
-	a_code[0x03] = a_code[0x03][0:76] + assemble_code(cf, cp_cache, side, 0, [
+	if mod.config.is_feature_enabled('block.smelter'): code.extend([
 		['ldc.class', 'com/thebluetropics/crabpack/SmelterBlockEntity'],
 		['ldc.string', 'Smelter'],
-		['invokestatic', c_name, 'a', '(Ljava/lang/Class;Ljava/lang/String;)V'],
-		'return'
+		['invokestatic', c_name, 'a', '(Ljava/lang/Class;Ljava/lang/String;)V']
 	])
 
-	# update code length
-	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
+	code.append('return')
+	a_code[0x03] = a_code[0x03][0:-1] + assemble_code(cf, cp_cache, side, len(a_code[0x03] - 1), code)
 
-	# remove line number table
+	a_code[0x02] = len(a_code[0x03]).to_bytes(4)
 	a_code[0x06] = (int.from_bytes(a_code[0x06]) - 1).to_bytes(2)
 
 	for i, a in a_code[0x07]:
@@ -39,8 +40,7 @@ def _modify_static_initializer(cf, cp_cache, side, c_name):
 			del a_code[0x07][i]
 			break
 
-	# update code attribute
-	a[0x02] = a_code_assemble(a_code)
+	a_code[0x06] = len(a_code[0x07]).to_bytes(2)
 
-	# update code attribute length
+	a[0x02] = a_code_assemble(a_code)
 	a[0x01] = len(a[0x02]).to_bytes(4)
