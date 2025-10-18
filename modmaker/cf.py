010481ba723bcb9cc4a2a852a53d80a7d1bdfc76
thebluetropics <thebluetropics@gmail.com>
2025-10-18 09:59:24 +0700

    rename `cf_bytes` to `buf`

--- modmaker/cf.py
+++ modmaker/cf.py
@@ -1,13 +1,13 @@
 from sys import stderr
 
-def cf_load(cf_bytes):
+def cf_load(buf):
 	cf = [None] * 16
 
-	cf[0x00] = cf_bytes[0:4]
-	cf[0x01] = cf_bytes[4:6]
-	cf[0x02] = cf_bytes[6:8]
+	cf[0x00] = buf[0:4]
+	cf[0x01] = buf[4:6]
+	cf[0x02] = buf[6:8]
 
-	cf[0x03] = cf_bytes[8:10]
+	cf[0x03] = buf[8:10]
 	cp_count = int.from_bytes(cf[0x03])
 
 	cf[0x04] = []
@@ -17,90 +17,90 @@ def cf_load(cf_bytes):
 	j = 1
 
 	while j < cp_count:
-		tag = cf_bytes[i:i + 1]
+		tag = buf[i:i + 1]
 
 		if tag.__eq__(b'\x07'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3]])
+			cp.append([j, tag, buf[i + 1:i + 3]])
 			i += 3
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x09'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3], cf_bytes[i + 3:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 3], buf[i + 3:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x0a'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3], cf_bytes[i + 3:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 3], buf[i + 3:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x0b'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3], cf_bytes[i + 3:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 3], buf[i + 3:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x08'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3]])
+			cp.append([j, tag, buf[i + 1:i + 3]])
 			i += 3
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x03'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x04'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x05'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 9]])
+			cp.append([j, tag, buf[i + 1:i + 9]])
 			i += 9
 			j += 2
 			continue
 
 		if tag.__eq__(b'\x06'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 9]])
+			cp.append([j, tag, buf[i + 1:i + 9]])
 			i += 9
 			j += 2
 			continue
 
 		if tag.__eq__(b'\x0c'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3], cf_bytes[i + 3:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 3], buf[i + 3:i + 5]])
 			i += 5
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x01'):
-			sz_b = cf_bytes[i + 1:i + 3]
+			sz_b = buf[i + 1:i + 3]
 			sz = int.from_bytes(sz_b)
-			cp.append([j, tag, sz_b, cf_bytes[i + 3:i + 3 + sz]])
+			cp.append([j, tag, sz_b, buf[i + 3:i + 3 + sz]])
 			i += 3 + sz
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x0f'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 2], cf_bytes[i + 2:i + 4]])
+			cp.append([j, tag, buf[i + 1:i + 2], buf[i + 2:i + 4]])
 			i += 4
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x10'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3]])
+			cp.append([j, tag, buf[i + 1:i + 3]])
 			i += 3
 			j += 1
 			continue
 
 		if tag.__eq__(b'\x12'):
-			cp.append([j, tag, cf_bytes[i + 1:i + 3], cf_bytes[i + 3:i + 5]])
+			cp.append([j, tag, buf[i + 1:i + 3], buf[i + 3:i + 5]])
 			i += 5
 			j += 1
 			continue
@@ -108,11 +108,11 @@ def cf_load(cf_bytes):
 		print('Err: unknown.', file=stderr)
 		exit(1)
 
-	cf[0x05] = cf_bytes[i:i + 2]
-	cf[0x06] = cf_bytes[i + 2:i + 4]
-	cf[0x07] = cf_bytes[i + 4:i + 6]
+	cf[0x05] = buf[i:i + 2]
+	cf[0x06] = buf[i + 2:i + 4]
+	cf[0x07] = buf[i + 4:i + 6]
 
-	cf[0x08] = cf_bytes[i + 6:i + 8]
+	cf[0x08] = buf[i + 6:i + 8]
 	interface_count = int.from_bytes(cf[0x08])
 	i += 8
 
@@ -120,10 +120,10 @@ def cf_load(cf_bytes):
 	interfaces = cf[0x09]
 
 	for _ in range(interface_count):
-		interfaces.append(cf_bytes[i:i + 2])
+		interfaces.append(buf[i:i + 2])
 		i += 2
 
-	cf[0x0a] = cf_bytes[i:i + 2]
+	cf[0x0a] = buf[i:i + 2]
 	f_count = int.from_bytes(cf[0x0a])
 	i += 2
 
@@ -132,14 +132,14 @@ def cf_load(cf_bytes):
 
 	for _ in range(f_count):
 		f = [
-			cf_bytes[i:i + 2],
-			cf_bytes[i + 2:i + 4],
-			cf_bytes[i + 4:i + 6],
+			buf[i:i + 2],
+			buf[i + 2:i + 4],
+			buf[i + 4:i + 6],
 			None,
 			None
 		]
 
-		a_count_b = cf_bytes[i + 6:i + 8]
+		a_count_b = buf[i + 6:i + 8]
 		f[0x03] = a_count_b
 		i += 8
 		a_count = int.from_bytes(a_count_b)
@@ -148,18 +148,18 @@ def cf_load(cf_bytes):
 
 		for _ in range(a_count):
 			a = [
-				cf_bytes[i:i + 2],
-				cf_bytes[i + 2:i + 6],
+				buf[i:i + 2],
+				buf[i + 2:i + 6],
 				None
 			]
 			sz = int.from_bytes(a[0x01])
-			a[0x02] = cf_bytes[i + 6:i + 6 + sz]
+			a[0x02] = buf[i + 6:i + 6 + sz]
 			attributes.append(a)
 			i += 6 + sz
 
 		fields.append(f)
 
-	cf[0x0c] = cf_bytes[i:i + 2]
+	cf[0x0c] = buf[i:i + 2]
 	m_count = int.from_bytes(cf[0x0c])
 	i += 2
 
@@ -168,14 +168,14 @@ def cf_load(cf_bytes):
 
 	for _ in range(m_count):
 		m = [
-			cf_bytes[i:i + 2],
-			cf_bytes[i + 2:i + 4],
-			cf_bytes[i + 4:i + 6],
+			buf[i:i + 2],
+			buf[i + 2:i + 4],
+			buf[i + 4:i + 6],
 			None,
 			None
 		]
 
-		a_count_b = cf_bytes[i + 6:i + 8]
+		a_count_b = buf[i + 6:i + 8]
 		m[0x03] = a_count_b
 		i += 8
 		a_count = int.from_bytes(a_count_b)
@@ -184,18 +184,18 @@ def cf_load(cf_bytes):
 
 		for _ in range(a_count):
 			a = [
-				cf_bytes[i:i + 2],
-				cf_bytes[i + 2:i + 6],
+				buf[i:i + 2],
+				buf[i + 2:i + 6],
 				None
 			]
 			sz = int.from_bytes(a[0x01])
-			a[0x02] = cf_bytes[i + 6:i + 6 + sz]
+			a[0x02] = buf[i + 6:i + 6 + sz]
 			attributes.append(a)
 			i += 6 + sz
 
 		methods.append(m)
 
-	cf[0x0e] = cf_bytes[i:i + 2]
+	cf[0x0e] = buf[i:i + 2]
 	a_count = int.from_bytes(cf[0x0e])
 	i += 2
 
@@ -204,12 +204,12 @@ def cf_load(cf_bytes):
 
 	for _ in range(a_count):
 		a = [
-			cf_bytes[i:i + 2],
-			cf_bytes[i + 2:i + 6],
+			buf[i:i + 2],
+			buf[i + 2:i + 6],
 			None
 		]
 		sz = int.from_bytes(a[0x01])
-		a[0x02] = cf_bytes[i + 6:i + 6 + sz]
+		a[0x02] = buf[i + 6:i + 6 + sz]
 		attributes.append(a)
 		i += 6 + sz
 
@@ -222,47 +222,47 @@ def load_class_file(path):
 	return cf
 
 def cf_assemble(cf):
-	cf_bytes = bytearray()
+	buf = bytearray()
 
-	cf_bytes.extend(cf[0x00])
-	cf_bytes.extend(cf[0x01])
-	cf_bytes.extend(cf[0x02])
-	cf_bytes.extend(cf[0x03])
+	buf.extend(cf[0x00])
+	buf.extend(cf[0x01])
+	buf.extend(cf[0x02])
+	buf.extend(cf[0x03])
 
 	for entry in cf[0x04]:
-		cf_bytes.extend(b''.join(entry[1:]))
+		buf.extend(b''.join(entry[1:]))
 
-	cf_bytes.extend(cf[0x05])
-	cf_bytes.extend(cf[0x06])
-	cf_bytes.extend(cf[0x07])
-	cf_bytes.extend(cf[0x08])
+	buf.extend(cf[0x05])
+	buf.extend(cf[0x06])
+	buf.extend(cf[0x07])
+	buf.extend(cf[0x08])
 
 	for inf in cf[0x09]:
-		cf_bytes.extend(inf)
+		buf.extend(inf)
 
-	cf_bytes.extend(cf[0x0a])
+	buf.extend(cf[0x0a])
 
 	for f in cf[0x0b]:
-		cf_bytes.extend(b''.join(f[0x00:0x04]))
+		buf.extend(b''.join(f[0x00:0x04]))
 
 		for a in f[0x04]:
-			cf_bytes.extend(b''.join(a))
+			buf.extend(b''.join(a))
 
-	cf_bytes.extend(cf[0x0c])
+	buf.extend(cf[0x0c])
 
 	for m in cf[0x0d]:
-		cf_bytes.extend(b''.join(m[0x00:0x04]))
+		buf.extend(b''.join(m[0x00:0x04]))
 
 		for a in m[0x04]:
-			cf_bytes.extend(b''.join(a))
+			buf.extend(b''.join(a))
 
-	cf_bytes.extend(cf[0x0e])
+	buf.extend(cf[0x0e])
 
 	for a in cf[0xf]:
-		cf_bytes.extend(b''.join(a))
+		buf.extend(b''.join(a))
 
-	cf_bytes = bytes(cf_bytes)
-	return cf_bytes
+	buf = bytes(buf)
+	return buf
 
 def cf_create():
 	cf = [None] * 16
